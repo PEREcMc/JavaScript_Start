1d465de21d2d2ed8ef26f7102d58eb901cf98241
PEREcMC <perecvlasenko@gmail.com>
2022-06-21 19:23:38 +0300

    added different task (polindrom, fizzbuzz, search vowels, n- number in fibonacci

--- app.js
+++ app.js
@@ -1,19 +1,16 @@
-// const text = 'Здорова, Этот текст будет отображаться наоборот.'
-// const reverseText = text.split('').reverse().join('')
-// console.log(reverseText)
-
+ const text = 'Здорова, Этот текст будет отображаться наоборот.'
+ const reverseText = text.split('').reverse().join('')
+ console.log(reverseText)
+//
 const dogs = ['Mira', 'Nelya', 'Zuchka']
-
 const dogstUpperCase = dogs.map(dog => {
     return dog.toUpperCase()
 })
 console.log(dogstUpperCase)
 //
 const numbers = [1, 2, 3, 4, 5, 10, 20, 30, 40, 50]
-
 const pow2 = num => num ** 2
 const pow2Numbers = numbers.map(pow2)
-
 console.log(pow2Numbers)
 //
 const filteredNumbers = pow2Numbers.filter(num => num > 100)
@@ -98,5 +95,99 @@ function isPositive(num) {
 }    
 console.log(arr3.every(isPositive))
 console.log(arr3.some(isPositive))
+//
+const users = [
+    {name: 'Ivan', tel: 89508632875, cach: 100000 },
+    {name: 'Petr', tel: 89508634875, cach: 67000 },
+    {name: 'Fedya', tel: 89548637875, cach: 25500 },
+]
+const allname = users
+.filter(person => person.name.includes('I'))
+.reduce(function(acc, person) {
+    acc = person.name.toLowerCase()
+    return acc
+}, 0)
+
+console.log(allname)
+//
+let answer = []
+questions = ['Ваше имя?', 'Ваша профессия?', 'Сколько вам лет?', 'Где вы живэте?']
+for (let i = 0; i < questions.lenght; i++) {
+    answer[i] = prompt(questions[i])
+}
+document.write(answer)
+
+// Полиндром - задача
+
+//let str = "Retro";
+let str = "Оголи жопу пожилого";
+const polindrom = str => {
+    str = str.toLowerCase().replace(/ /g, '')
+    return str === str.split('').reverse().join('')
+}
+console.log(polindrom(str))
+
+// FizzBuzz - задача
+
+const fizzBuzz = num => {
+    for (let i = 1; i <= num; i++) {
+        // chek if the number is a multiple of 3 and 5
+       if (i % 3 === 0 && i % 5 === 0) {
+            console.log("fizzbuzz")
+        } // chek if the number is a multiple of 3 
+        else if (i % 3 === 0) {
+            console.log("fizz")
+        } // chek if the number is a multiple of 5
+        else if (i % 5 === 0) {
+            console.log("buzz")
+        } else {
+            console.log(i)
+        }
+    }
+}
+console.log(fizzBuzz(50))
+
+
+// Анаграма - задача (вернуться к ней позднее)
+
+
+// Поиск гласных - задача
+//гласными являются: 'a', 'e', 'i', 'o', 'u'
+
+const findVowels = str => {
+    let count = 0
+    const vowels = ['a', 'e', 'i', 'o', 'u']
+    for (let char of str.toLowerCase()) {
+        if(vowels.includes(char)) {
+            count++
+        }
+    }
+    return count 
+}
+console.log(findVowels('The London of ...'))
+
+// другой способ
+
+// const findVowels = str => {
+//     const matched = str.match(/[aeiou]/gi)
+//     return matched ? matched.lenght : 0
+// }
+// console.log(findVolwes('the pipetsc'))  ?????
+
+
+// Последовательность Фибоначи - задача
+
+const fibonacci = num => {
+    const result = [0, 1]
+    for (let i = 2; i <= num; i++) {
+        const prevNum1 = result[i - 1]
+        const prevNum2 = result[i - 2]
+        result.push(prevNum1 + prevNum2)
+    }
+    return result[num]
+}
+console.log(fibonacci(3))
+
+
 
 
