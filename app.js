80504540f7db7f61624b5dde436d60e56b7ebcef
PEREcMC <perecvlasenko@gmail.com>
2022-06-24 22:34:12 +0300

    function

--- app.js
+++ app.js
@@ -357,3 +357,39 @@ function name(params) {
 }
 
 
+// Имя функции: глагольный префикс + уточнение
+// get Age() - возвращает возраст, showMessage() - показывает сообщение, checkPermission - проверяет доступ.
+
+function checkAge(age) {
+   return (age > 18) ? true : confirm('Родители разрешили?')
+}
+//
+function checkAge(age) {
+    return (age > 18) || confirm('Родители разрешили?')
+ }
+//
+ function getMin(a, b) {
+       if (a < b) {
+           return a
+       } else {
+           return b
+       }
+ }
+ getMin(4,7)
+//
+ function pow(x, n) {
+    let result = x
+    for (let i = 1; i < n; i++) {
+        result *= x
+    }
+    return result
+ }
+
+ let x = prompt('Введите число x:', '')
+ let n = prompt('Введите число y:', '')
+
+ if (n < 1) {
+     alert(`Степень ${n} не поддерживается, используйте натурольное число`)
+ } else {
+     alert(pow(x, n))
+ }
